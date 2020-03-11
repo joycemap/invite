1aeb25be63e869d811812ab48d03a359d9fe45dd
joycemap <57893220+joycemap@users.noreply.github.com>
2020-03-11 21:59:16 +0800

    change to heroku from local

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ passport.use(
             clientID: process.env.clientID,
             clientSecret: process.env.clientSecret,
             callbackURL:
-                // "https://referfriends.herokuapp.com/auth/facebook/callback",
-                "http://localhost:5000/auth/facebook/callback",
+                "https://referfriends.herokuapp.com/auth/facebook/callback",
+            // "http://localhost:5000/auth/facebook/callback",
             profileFields: ["id", "displayName", "photos", "email"],
             enableProof: true
         },
